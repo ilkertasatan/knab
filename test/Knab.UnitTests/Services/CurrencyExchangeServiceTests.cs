cd34faaad48cfe280f8b6a9bf6b1ca95c470e42d
Ilker Tasatan <Ilker.Tasatan@effectory.com>
2022-06-14 23:55:28 +0200

    Add unit tests

---
+++ test/Knab.UnitTests/Services/CurrencyExchangeServiceTests.cs
@@ -0,0 +1,36 @@
+﻿using FluentAssertions;
+using Knab.QuoteService.Domain.ValueObjects;
+using Knab.QuoteService.Infrastructure.CurrencyExchange;
+using Xunit;
+
+namespace Knab.UnitTests.Services;
+
+public class CurrencyExchangeServiceTests
+{
+    [Fact]
+    public async Task Should_Convert_Price_To_Given_Rates()
+    {
+        var (amount, _) = new Price(5, Currency.Usd);
+        var expectedPrices = new List<Price>
+        {
+            new((decimal)1.457322 * amount, Currency.Aud),
+            new((decimal)5.139496 * amount, Currency.Brl),
+            new((decimal)0.96034 * amount, Currency.Eur),
+            new((decimal)0.83501 * amount, Currency.Gbp),
+            new(1 * amount, Currency.Usd),
+        };
+        var sut = new CurrencyExchangeServiceFake();
+
+        var actualPrices = await sut.GetExchangeRate(new Price(5, Currency.Usd),
+            new[]
+            {
+                Currency.Aud,
+                Currency.Eur,
+                Currency.Gbp,
+                Currency.Brl,
+                Currency.Usd
+            });
+
+        actualPrices.Should().NotBeNullOrEmpty().And.Contain(expectedPrices);
+    }
+}
