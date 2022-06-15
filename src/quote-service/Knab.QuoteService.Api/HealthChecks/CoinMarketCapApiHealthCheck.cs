d70ffcbd2f5e279ab10c38343e96650871c1932d
Ilker Tasatan <Ilker.Tasatan@effectory.com>
2022-06-15 13:33:24 +0200

    Add health check for CoinMarketCapApi

---
+++ src/quote-service/Knab.QuoteService.Api/HealthChecks/CoinMarketCapApiHealthCheck.cs
@@ -0,0 +1,28 @@
+﻿using Knab.QuoteService.Application.Services;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace Knab.QuoteService.Api.HealthChecks;
+
+public class CoinMarketCapApiHealthCheck : IHealthCheck
+{
+    private readonly ICryptoCurrencyService _cryptoCurrencyService;
+
+    public CoinMarketCapApiHealthCheck(ICryptoCurrencyService cryptoCurrencyService)
+    {
+        _cryptoCurrencyService = cryptoCurrencyService;
+    }
+
+    public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+    {
+        try
+        {
+            await _cryptoCurrencyService.GetCryptoCurrencyAsync("BTC");
+        }
+        catch (Exception ex)
+        {
+            return HealthCheckResult.Unhealthy($"{nameof(CoinMarketCapApiHealthCheck)}: Exception during check: {ex.GetType().FullName}", ex);
+        }
+        
+        return HealthCheckResult.Healthy($"{nameof(CoinMarketCapApiHealthCheck)}: Healthy");
+    }
+}
