44bdb0bddbd4e8b8df043a76c6f254844fe0a8f9
Ilker Tasatan <Ilker.Tasatan@effectory.com>
2022-06-15 13:32:12 +0200

    Add health check for CoinMarketCapApi

--- src/quote-service/Knab.QuoteService.Api/Extensions/HealthCheckExtensions.cs
+++ src/quote-service/Knab.QuoteService.Api/Extensions/HealthCheckExtensions.cs
@@ -8,7 +8,8 @@ public static class HealthCheckExtensions
     public static IServiceCollection AddHealthCheck(this IServiceCollection services)
     {
         services.AddHealthChecks().AddCheck<LivenessHealthCheck>("Liveness", HealthStatus.Unhealthy);
-        services.AddHealthChecks().AddCheck<ExchangeRateApiHealthCheck>("Readiness", HealthStatus.Unhealthy);
+        services.AddHealthChecks().AddCheck<ExchangeRatesDataApiHealthCheck>("ExchangeRatesDataApiReadiness", HealthStatus.Unhealthy);
+        services.AddHealthChecks().AddCheck<CoinMarketCapApiHealthCheck>("CoinMarketCapApiReadiness", HealthStatus.Unhealthy);
             
         return services;
     }
